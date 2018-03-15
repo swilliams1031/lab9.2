1cb1d2efd48043732d0731c9d3a10d4d30ca22c6
Steve Williams <swilliams303@student.cccs.edu>
2018-03-14 20:22:42 -0600

    add State Parks JavaScript from JSbin

--- js/lab9.2.js
+++ js/lab9.2.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+//document.addEventListener('DOMContentLoaded', function() {
     /*
     your code will go here
     this is the event callback function that is called when the document content has been loaded
@@ -6,4 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
     *this is new as js bin takes care of this for you
     */
     
-});
+//});
+
+var map = L.map('map').setView([39.5501, -105.7821], 7);
+  L.esri.basemapLayer('ImageryClarity').addTo(map);
+  L.esri.basemapLayer('ImageryTransportation').addTo(map);
+  L.esri.basemapLayer('ImageryLabels').addTo(map);
+ 
+
+var stateParks = L.esri.featureLayer({
+  url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/CPW_Weeds_Trt_Track/FeatureServer/1',
+  style: function () {
+          return { color: "#e2f310", weight: 3
+          };
+         }
+  }).addTo(map);
