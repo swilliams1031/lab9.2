08c6f8fe6694677abb273a8bca1d76ac4f04d559
Steve Williams <swilliams303@student.cccs.edu>
2018-03-14 20:29:28 -0600

    indent html for legibility

--- js/lab9.2.js
+++ js/lab9.2.js
@@ -1,23 +1,23 @@
 //document.addEventListener('DOMContentLoaded', function() {
-    /*
+/*
     your code will go here
     this is the event callback function that is called when the document content has been loaded
     we need our map container to be ready before dropping our map in, for example
     *this is new as js bin takes care of this for you
     */
-    
+
 //});
 
 var map = L.map('map').setView([39.5501, -105.7821], 7);
-  L.esri.basemapLayer('ImageryClarity').addTo(map);
-  L.esri.basemapLayer('ImageryTransportation').addTo(map);
-  L.esri.basemapLayer('ImageryLabels').addTo(map);
- 
+L.esri.basemapLayer('ImageryClarity').addTo(map);
+L.esri.basemapLayer('ImageryTransportation').addTo(map);
+L.esri.basemapLayer('ImageryLabels').addTo(map);
+
 
 var stateParks = L.esri.featureLayer({
-  url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/CPW_Weeds_Trt_Track/FeatureServer/1',
-  style: function () {
-          return { color: "#e2f310", weight: 3
-          };
-         }
-  }).addTo(map);
+    url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/CPW_Weeds_Trt_Track/FeatureServer/1',
+    style: function () {
+        return { color: "#e2f310", weight: 3
+               };
+    }
+}).addTo(map);
