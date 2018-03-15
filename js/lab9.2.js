0c31fec6ea4a42b6426d12f2ab66eac56925119f
Steve Williams <swilliams303@student.cccs.edu>
2018-03-14 20:16:18 -0600

    Initial commit

---
+++ js/lab9.2.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    /*
+    your code will go here
+    this is the event callback function that is called when the document content has been loaded
+    we need our map container to be ready before dropping our map in, for example
+    *this is new as js bin takes care of this for you
+    */
+    
+});
